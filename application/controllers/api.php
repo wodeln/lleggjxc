315ab50639a01ea8678be0b6c1d51233cbb4f428
wodeln <Lina521521>
2017-10-09 14:29:55 +0800

    商品接口

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -7,27 +7,11 @@ class Api extends CI_Controller {
 //		$this->common_model->checkpurview();
     }
 	
-    //其他入库列表高级搜索
-	public function other_search() {
-		$this->load->view('storage/other-search');	
-	}
-	
-	//盘点
-	public function inventory() {
-	    $this->common_model->checkpurview(11);
-	    $this->load->view('storage/inventory');	
-	}
-	
-	//其他入库列表高级搜索
-	public function transfers_search () {
-	    $this->load->view('storage/transfers-search');	
-	}
-	
-	//盘点导入
-	public function import () {
-	    $this->load->view('storage/import');	
-	}
-	
+   //用户接口开始
+
+    /**
+     * 获取所有用户
+     */
 	public function getAllUsers(){
         $usersJson = file_get_contents('http://www.llegg.com/index.php/Api/Jxcapi/getAllUser');
         $users=json_decode($usersJson,TRUE);
@@ -55,35 +39,86 @@ class Api extends CI_Controller {
                 $address[$key]["id"]= $value["address_id"];
             }
             $data['linkMans']=json_encode($address,JSON_UNESCAPED_UNICODE);
-            $this->save_log($data,__FUNCTION__);
+            $this->save_log($data,"user",__FUNCTION__);
             $this->mysql_model->insert("ci_contact",$data);
         }
     }
 
+    /**
+     * 添加用户
+     */
     public function insertUser(){
         $data = $_POST;
-        $this->save_log($data,__FUNCTION__);
+        $this->save_log($data,"user",__FUNCTION__);
         $res = $this->mysql_model->insert("ci_contact",$data);
         echo $res;
     }
 
+    /**
+     * 编辑用户
+     */
     public function updateUser(){
         $data = $_POST;
-        $this->save_log($data,__FUNCTION__);
+        $this->save_log($data,"user",__FUNCTION__);
         $where = "(shop_user_id=".$data['userId'].")";
         unset($data["userId"]);
         $res = $this->mysql_model->update("ci_contact",$data,$where);
         echo $res;
     }
+    //用户接口结束
 
-    function save_log($res,$functionName="",$url="") {
+    //商品接口开始
+    /**
+     * 添加商品
+     */
+    public function insertGoods(){
+        $data = $_POST;
+
+        /*$data = array (
+            'name' => iconv('GB2312', 'UTF-8','土鸡蛋/箱/净重48斤/420枚/称差≤0kg/破损≤5枚'),
+            'number' => 'JDT00001',
+            'unitName' => iconv('GB2312', 'UTF-8','箱装'),
+            'categoryName' => iconv('GB2312', 'UTF-8','土鸡蛋 纸箱'),
+            'shop_goods_id' => '2',
+        );*/
+
+
+        $this->save_log($data,"goods",__FUNCTION__);
+        $this->load->library('lib_cn2pinyin');
+        $data['pinYin'] = $this->lib_cn2pinyin->encode($data['name']);
+        $data['baseUnitId'] = $this->mysql_model->get_results(UNIT,"(`name` = '".$data['unitName']."')",'id')[0]['id'];
+        $data['categoryId'] = $this->mysql_model->get_results(CATEGORY,"(`name` = '".$data['categoryName']."')",'id')[0]['id'];
+        $res = $this->mysql_model->insert(GOODS,$data);
+        echo $res;;
+    }
+
+    public function updateGoods(){
+        $data = $_POST;
+        $this->save_log($data,"goods",__FUNCTION__);
+        $this->load->library('lib_cn2pinyin');
+        $data['pinYin'] = $this->lib_cn2pinyin->encode($data['name']);
+        $data['baseUnitId'] = $this->mysql_model->get_results(UNIT,"(`name` = '".$data['unitName']."')",'id')[0]['id'];
+        $data['categoryId'] = $this->mysql_model->get_results(CATEGORY,"(`name` = '".$data['categoryName']."')",'id')[0]['id'];
+        $where = "(shop_goods_id=".$data['shop_goods_id'].")";
+        $res = $this->mysql_model->update(GOODS,$data,$where);
+        echo $res;
+    }
+
+    //商品接口结束
+    /**
+     * 日志方法
+     * @param $res 传递数组详情
+     * @param string $functionName 调用接口方法名称
+     * @param string $url 调用接口URL
+     */
+    function save_log($res,$type,$functionName="",$url="") {
         $date = date("Y-m-d", time());
         //$address = '/var/log/error';
         $address = './application/apilog';
         if (!is_dir($address)) {
             mkdir($address, 0777, true);
         }
-        $address = $address.'/'.$date . '_data.log';
+        $address = $address.'/'.$date . '_'.$type.'.log';
         $error_date = date("Y-m-d H:i:s", time());
         if(!empty($_SERVER['HTTP_REFERER'])) {
             $file = $_SERVER['HTTP_REFERER'];
